73a0e32a5cbf36ac0cd101c2b18625dc990ac79a
Adrien <adriengonz@outlook.fr>
2023-11-10 12:45:30 +0100

    Ajout d'une page html au serveur

--- main.go
+++ main.go
@@ -4,15 +4,35 @@ import (
 	"fmt"
 	//"time"
 	"net/http"
-
+	"html/template"
 )
+
+type GameData struct {
+	OriginalWord     string
+	ToFind           string
+	Attempts         int
+	LettersSuggested []string
+	Error            string
+	HangmanPositions [10]string
+}
+
 func handler(w http. ResponseWriter, r *http. Request) {
-	switch r.URL.Path {
+	switch r.URL.Path { // Execute un code block différent selon l'URL demandée
 	case "/home":
 		fmt.Fprintf(w, "tkt")
+	case "/test":
+		renderTemplate(w, "default")
 	default:
 		fmt.Fprintf(w, "Default Page")
 	}
+}
+
+func renderTemplate (w http.ResponseWriter, htmlfile string) {
+	t, err := template.ParseFiles("./templates/" + htmlfile + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	t.Execute(w, nil)
 }
 
 var hidden_word []string // Variable sous forme de liste qui contient le mot caché
@@ -20,9 +40,10 @@ var used_letters []string // Variable sous forme de liste qui contient les lettr
 
 func main() {
 	fmt.Println("Server running on port 8080")
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/home", handler)
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Access: http://localhost:8080")
+	http.HandleFunc("/", handler) // Initilaise la page par défaut
+	http.HandleFunc("/home", handler) // Initialise la page /home
+	http.ListenAndServe(":8080", nil) // Lance le serveur sur le port 8080
 	/*
 	PrintHangmanAscii() // Appel de la fonction pour afficher "Hangman" en ascii
 	fmt.Println("Bienvenue sur Hangman !")
